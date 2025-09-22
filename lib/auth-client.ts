5c26eff56477d3cf8ad13ec7a3bca45955713b16
ocitd <rasyidabqarihasibuan@gmail.com>
2025-09-23 05:21:58 +0700

    revisi230925

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    // baseURL: "https://rasyidabqarihasibuan.my.id"
-    baseURL: "http://localhost:3000"
+    baseURL: "https://rasyidabqarihasibuan.my.id"
+    // baseURL: "http://localhost:3000"
 })
 export const { signIn, signUp, useSession } = createAuthClient()
