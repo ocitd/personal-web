2b48615c32f667dffef67e83e14b98c55431d280
ocitd <rasyidabqarihasibuan@gmail.com>
2025-09-23 09:37:05 +0700

    Update page.tsx
    cv page

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,7 +1,16 @@
 export default function page() {
   return (
     <div>
-      this cv page
+      <h1>CV</h1>
+      <p>This is the CV page.</p>
+      <p>PDF preview</p>
+      <iframe
+        src="/Rasyid-Abqari-Hasibuan-CV.pdf"
+        width="100%"
+        height="600px"
+        style={{ border: 'none' }}
+        title="CV PDF"
+      ></iframe>
     </div>
   )
 }
